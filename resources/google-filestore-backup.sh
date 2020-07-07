ad1ac20df6eb7eff612811b82dc19146edf171ee
sreesanpd <sreesanpd@users.noreply.github.com>
2020-07-08 01:23:07 +0200

    Create google-filestore-backup.sh

---
+++ resources/google-filestore-backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#install nfs-common
+
+apt install nfs-common -y
+
+apt install rsync -y
+
+source /root/.bashrc
+
+
+#backup filestore to GCS
+
+DATE=$(date +"%m-%d-%Y-%T")
+
+mkdir -p /mnt/$FILESHARE_MOUNT_PRIMARY
+mount $FILESTORE_IP_PRIMARY:/$FILESHARE_NAME_PRIMARY /mnt/$FILESHARE_MOUNT_PRIMARY
+
+gsutil rsync -r /mnt/$FILESHARE_MOUNT_PRIMARY/ gs://$GCP_BUCKET_NAME/$DATE/
+
+
+#rsync filestore to secondary region
+
+mkdir -p /mnt/$FILESHARE_MOUNT_SECONDARY
+mount $FILESTORE_IP_SECONDARY:/$FILESHARE_NAME_SECONDARY /mnt/$FILESHARE_MOUNT_SECONDARY
+
+rsync -avz /mnt/$FILESHARE_MOUNT_PRIMARY/ /mnt/$FILESHARE_MOUNT_SECONDARY/
+
+#sleep for few minutes - for testing the script ## 
+
+#sleep 300
+
+
+#stop the VM
+
+poweroff
